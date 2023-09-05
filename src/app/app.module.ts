4cedb1f2c32bef3d2199d840926bbdad6dc9d090
FelipeJSousa <csfelipe8@gmail.com>
2023-09-04 22:06:03 -0300

    feat(project): init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RulerComponent } from './ruler/ruler.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RulerComponent
   ],
   imports: [
     BrowserModule,
